9fb537ed12b1a2a01fdb2ff5e18d498890fa9a0e
Corbett Baratta <cjbaratta@outlook.com>
2025-01-16 19:21:58 -0600

    Set game clock

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -11,6 +13,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 if __name__ == '__main__':
     main()
